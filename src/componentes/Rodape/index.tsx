1bf8de98f721057aacdaae7befe1c49fa6521c32
Carlos-Eduardo-Kadu <kadumeloduarte@gmail.com>
2025-08-02 14:28:25 -0300

    Componente para representar o rodapé

---
+++ src/componentes/Rodape/index.tsx
@@ -0,0 +1,12 @@
+import "./style.css"
+export default function Rodape(){
+    return(
+
+        <footer>
+            <div className="container-footer">
+                <p>@dscarrostop</p>
+                <p>Rua da Pipoca, 150</p>
+            </div>
+        </footer>
+    );
+}
--- src/routes/PaginaCarros/index.tsx
+++ src/routes/PaginaCarros/index.tsx
@@ -1,10 +1,12 @@
 import CardCarro from "../../componentes/CardCarro";
 import HearderCar from "../../componentes/HeaderCar";
+import Rodape from "../../componentes/Rodape";
 export default function PaginaCarros() {
     return (
         <>
             <HearderCar />
             <CardCarro/>
+            <Rodape/>
         </>
     );
 }
